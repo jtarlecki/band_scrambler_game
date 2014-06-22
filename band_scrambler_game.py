d242514f420171ecab822868feaf8215fbbc843a
jtarlecki <jtarlecki@yahoo.com>
2014-06-22 01:51:40 -0400

    new printed scoreboard, lots of notes at the top of the module for future work

--- band_scrambler_game.py
+++ band_scrambler_game.py
@@ -4,43 +4,147 @@ import json
 from random import randint
 from private import *
 
+'''
+// TODO:
+
+title screen
+
+compare player1's score to the highscore,
+if any are higher, prompt them for their name (under 8 digits)
+and write it to the database (aka, json to .txt file)
+
+break out some of the generic class manipulations from 
+HighScore() class, they acaully have nothing to do with the 
+class and are generic functions that could be used elsewhere
+
+consolodate screen feedback into one area
+
+make a Scoreboard.refresh() method to clean up updates to the board
+clean up the code in this class
+
+in Scoreboard class-- dynamic inputs of player1 and hi_score lists?
+these could technically come from the classes themselves, in the right
+order.. perhpas using some of the generic functions that need to 
+broke out of the HighScore() class
+
+organize this into a package.  It doens't need to be in one module.
+
+dynamic reading of mp3 files?
+
+store json of the entire music library locally??
+allow user to refresh?
+or allow user to define where the library is?
+make a class to atleast allow for this in future
+if i change the front end?
+'''
+
+
+class Scoreboard():
+	
+	def __init__(self, scorecard, highscore):
+		self.player1 = scorecard
+		self.highscore = highscore
+		self.stars = '*'*75
+		self.dashes = '-'*75
+		self.line = '_'*75
+		
+	def refresh(self):
+		# at the end of each round, this should control the updates
+		pass
+
+	def results(self):
+		# // TODO: this might get the axe
+		# with a running scoreboard, this is just redundant
+		print ''
+		print 'Your game stats'
+		print self.dashes
+		print 'Games won: %d / %d' % (self.player1.wins, self.player1.rounds_played)
+		print 'Correct guess percentage: %s' % (self.player1.correct_guess_percent)
+		print self.dashes
+		print ''
+
+	def print_header(self, cheat='*'):
+		
+		def stringify_list(mylist):
+			if type(mylist) == type([]):
+				mylist = ''.join([pad_string(str(item)) for item in mylist])
+			return mylist
+			
+		def pad_string(string):
+			while len(string)<9:
+				string = ' ' + string
+			return pipe_cap(string)
+		
+		def pipe_cap(string):
+			return string+' |'
+		
+		headers = ['','ROUNDS', 'WINS' , 'GUESSES', 'WIN %', 'GUESS %']
+		player1= ['PLAYER 1',
+		          self.player1.rounds_played,
+		          self.player1.wins,
+		          self.player1.guess_count,
+		          self.player1.win_percent,
+		          self.player1.correct_guess_percent
+		          ]
+		hi_score = ['HI SCORE',
+		           self.highscore.scores.rounds_played,
+		           self.highscore.scores.wins,
+		           self.highscore.scores.guess_count,
+		           self.highscore.scores.win_percent,
+		           self.highscore.scores.correct_guess_percent
+		           ]
+		
+		line = '='*len(stringify_list(headers))
+		the_board = [headers, line, player1, hi_score, line]
+		
+		for item in the_board:
+			print stringify_list(item)
+
+		print ''
+		print self.stars
+		print 'GAME INSTRUCTIONS:'
+		print ''
+		print ' - Type in the correct spelling of the band.'
+		print ' - You have three (3) chances to correctly guess.'
+		print ' - Capitalization does NOT matter.'
+		print ''
+		print ' * You can type "%s" to re-scramble your clue.' % (cheat)
+		print '   (However, it is only possilbe on your first guess'
+		print '   as you will lose a guess when you re-scramble!!)'
+		print ''
+		print self.stars
+		print '\n'
+	
+		
+
 class Scorecard():
 	
-	def __init__(self, wins=0, rounds_played=0, guess_count=0, correct_guess_percent='0.00%'):
-		self.wins = wins
+	def __init__(self, 
+	             rounds_played=0, 
+	             wins=0, 
+	             guess_count=0, 
+	             win_percent='0.00%', 
+	             correct_guess_percent='0.00%'):
+		
 		self.rounds_played = rounds_played
+		self.wins = wins
 		self.guess_count = guess_count
+		self.win_percent = win_percent
 		self.correct_guess_percent = correct_guess_percent
 
 	def update(self, guesses, won_game):
-		self.rounds_played += 1
-		self.guess_count += guesses
-		self.wins += int(won_game)	
-		self.correct_guess_percent = "%.2f" % ((float(self.wins)*100)/self.guess_count) + '%'
-	
-	def results(self):
-		print ''
-		print 'Your game stats'
-		print '-----------------------------'
-		print 'Games won: %d / %d' % (self.wins, self.rounds_played)
-		print 'Correct guess percentage: %s' % (self.correct_guess_percent)
-		print '-----------------------------'
-		print ''
-
-	def print_header(self, cheat):
-		print 'ROUND: %d' % (self.rounds_played+1), 'GAMES WON: %d' % (self.wins)
-		print 
-		print ''
-		print '*************************************************************************'
-		print 'GAME INSTRUCTIONS:\n'
-		print " - You have three (3) chances to correctly guess."
-		print " - Capitalization does NOT matter.\n - Please guess OR type '%s' to re-scramble your clue." % (cheat)
-		print "   (However, you will lose a guess if you re-scramble!!)"
-		print ''
-		print '*************************************************************************'
-		print '\n\n'
 		
-def play_game(Scorecard):
+		def calc_percent(num, denom):
+			return "%.2f" % ((float(num)*100)/denom) + '%'
+		
+		self.rounds_played += 1
+		self.wins += int(won_game)	
+		self.guess_count += guesses
+		self.win_percent = calc_percent(self.wins, self.rounds_played)
+		self.correct_guess_percent = calc_percent(self.wins, self.guess_count)
+
+		
+def play_game(scoreboard):
 	prompts = GamePrompts()
 	
 	s = Scrambler()
@@ -48,7 +152,7 @@ def play_game(Scorecard):
 	scram = s.scram
 	
 	clue = 'Here is your clue:'					# GamePromps class
-	cheat = '*'							# comments class
+	cheat = '?'							# comments class
 	lose_response = ['Better luck next time!',
 		'whooo boy, that was BRUTAL!',
 		'Seriously dude?',
@@ -66,7 +170,7 @@ def play_game(Scorecard):
 		"[one last try!]> "
 		]	
 
-	Scorecard.print_header(cheat)
+	scoreboard.print_header(cheat)
 	
 	print clue
 	print '\n     %s\n'% scram
@@ -96,8 +200,8 @@ def play_game(Scorecard):
 		print ''
 		won = False
 	
-	Scorecard.update(guesses, won)
-	Scorecard.results()
+	scoreboard.player1.update(guesses, won)
+	scoreboard.results()
 	
 	cont = raw_input('[Press "enter" to continue the game... or hit "Q" to quit]> ' )
 	if cont.upper() != 'Q':
@@ -169,61 +273,59 @@ class GamePrompts(object):
 		pass	
 
 class HighScores(object):
-# wins=0, rounds_played=0, guess_count=0, correct_guess_percent='0.00%'
+	'''
+	so many initializations of Scorecard()
+	think about passing that in as a parameter to this class.
+	I count five (5) instances
+	But there may not be any benefit
+	I may be going overboard with making everything generic.
+	'''
 	def __init__(self):
+		
+		# read_scores() and write_scores() methods close the log file
+
 		self.filename = 'high_scores.txt'
 		self.score_list = None
 		try:
-			#self.open_scores('r')
-			# // TODO: needs read method
 			self.read_scores()
-			self.unpack_json()
-			#self.score_log.close()
+			self.init_classes()
 		except:
 			self.open_scores('w')
-			self.scores = Scorecard()
-			self.owners = Scorecard(*4*['---'])
-			
-			# this is work in progress... 
-			# works great, just needs to be organized
-			self.create_json() # test call
-			
-			self.write_scores(self.json_scores_text)
-				
-			self.score_log.close()
-			
+			self.init_classes()
+
+	
 	def open_scores(self, mode='r'):
 		self.score_log = open('%s' % (self.filename), mode)		
 		
 	def read_scores(self):
 		self.open_scores('r')
 		self.json_scores_text = self.score_log.read()
-		print 'self.json_scores_text: ', self.json_scores_text
 		self.json_scores_python = json.loads(self.json_scores_text)
-		print 'json_scores_python: ', self.json_scores_python
 		self.score_log.close()
 		
-	def write_scores(self, score_list):
+		print 'self.json_scores_text: '
+		print self.json_scores_text
+		print 'json_scores_python: ', self.json_scores_python
+		
+	def write_scores(self, scores_json_format):
 		self.open_scores('w')
-		self.score_list = score_list
-		self.score_log.write(score_list)
+		self.score_log.write(scores_json_format)
 		self.score_log.close()
 	
 	def close(self):
 		self.score_log.close()
 	
 	def create_json(self):
-
-		created_json = {'scores': self.class_to_dict(self.scores),
-		                'owners': self.class_to_dict(self.owners)}
+		self.json_scores_python = {'scores': self.class_to_dict(self.scores),
+		                           'owners': self.class_to_dict(self.owners)}
+		self.json_scores_text = json.dumps(self.json_scores_python,
+		                                   sort_keys=True,
+		                                   indent=4,
+		                                   separators=(',', ': '))			
 		
-		print 'created_json', created_json
-		
-		print 'pretty print:'
-		print json.dumps(created_json, sort_keys=True, indent=4, separators=(',', ': '))
-		
-		self.json_scores_python = created_json
-		self.json_scores_text = json.dumps(created_json, sort_keys=True, indent=4, separators=(',', ': '))
+		print 'self.json_scores_python:', self.json_scores_python
+		print 'self.json_scores_text:'
+		print self.json_scores_text
 
 	def class_to_dict(self, cls):
 		# turns a class into a dictionary
@@ -232,27 +334,47 @@ class HighScores(object):
 			d[key] = cls.__dict__[key]
 		return d
 	
-	def unpack_json(self):
-		self.scores = Scorecard()
-		self.owners = Scorecard(*len(Scorecard().__dict__)*['---'])
+	def init_classes(self):
+		if self.json_scores_python == None:
+			self.scores = Scorecard()
+			self.owners = Scorecard(*len(Scorecard().__dict__)*['---'])
+			self.create_json() 
+			self.write_scores(self.json_scores_text)
+		else:
+			self.scores = self.dict_to_cls(self.json_scores_python['scores'], Scorecard())
+			self.owners = self.dict_to_cls(self.json_scores_python['owners'], Scorecard())
+			# json already exists
+			# scores came from database
 	
-	def sorted_cls_prop_list(self, cls):			
+	def cls_property_list_sorted(self, cls):			
+		'''
+		first, get a ranked list of class arguments as a list of tuples
+		then, sort the tuples by the rank number (asc)
+		finally, pull the sorted class property names out
+		and return them in an ordered list
+		'''
 		cls_numbered = cls.__class__(*[i for i in range(len(cls.__dict__))])
-		print 'cls_numbered.__dict__:', cls_numbered.__dict__
-
-		# ordered list of numbered class property tuples
 		cls_tuples = sorted(cls_numbered.__dict__.items(), key=lambda prop: prop[1])
-		print 'cls_tuples: ', cls_tuples
-		
 		cls_list = []
+		
 		for item in cls_tuples:
 			cls_list.append(item[0])
-		print 'cls_list: ', cls_list		
 		
-	def dict_to_class(self, dictionary, cls):
-		pass
+		return cls_list
+		
+	def dict_to_cls(self, dictionary, cls):
+		'''
+		pass in a dictionary and a dummy class instance
+		and get a new instance of that class,
+		instantiated with the arguments from that dictionary
+		'''
+		args = []
+		for cls_prop in self.cls_property_list_sorted(cls):
+			args.append(dictionary[cls_prop])
+		return cls.__class__(*args)
 		
 	### not used ###
+	'''
 	def unpack_scores(self):
 		self.highscores_list = []
 		self.owners_list = []
@@ -262,6 +384,7 @@ class HighScores(object):
 		highscores = Scorecard(*self.highscores_list)
 		owners = Scorecard(*self.owners_list)
 		print 'unpacked'
+	'''
 		
 class Engine(object):
 	
@@ -271,11 +394,11 @@ class Engine(object):
 	def start(self):
 		print '\n\n****NEW GAME***\n\n'
 		
-		s = Scorecard()
+		scoreboard = Scoreboard(Scorecard(),HighScores())
 
 		while True:
 			os.system('cls')
-			play_game(s)
+			play_game(scoreboard)
 	
 
 def run():
@@ -283,35 +406,9 @@ def run():
 
 def test():
 	# wins=0, rounds_played=0, guess_count=0, correct_guess_percent='0.00%'
-	'''
-	scores = {'scores': 
-	          {'wins': 0,
-	           'rounds_played': 0,
-	           'guess_count': 0, 
-	           'correct_guess_percent': '45.45%'
-	           },
-	          'owners':
-	          {'wins': "JT",
-	           'rounds_played': "DAD",
-	           'guess_count': "MOM", 
-	           'correct_guess_percent': "PEN"
-	           }
-	          }
-	
-	scores_json = json.dumps(scores, sort_keys=True, indent=4, separators=(',', ': '))
-	print scores_json
-	python_obj = json.loads(scores_json)
-	print python_obj['owners']
-	print python_obj['scores']
-	'''
 	h = HighScores()
-	#h.read_scores()
-	#h.unpack_scores()
 
-def test2():
-	h = HighScores()
-	#h.dict_to_class('','')
-	h.sorted_cls_prop_list(Scorecard())
-test2()
+if __name__ == '__main__':
+	run()
 
 
